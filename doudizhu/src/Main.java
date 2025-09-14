bdbdce1b976d428b7279e877dc47945f41291e1f
HerryLo <herryloyopai@163.com>
2025-09-14 21:44:41 +0800

    add doudizhu

---
+++ doudizhu/src/Main.java
@@ -0,0 +1,8 @@
+import com.liuheng.game.LoginJFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+       new LoginJFrame();
+    }
+}
---
+++ doudizhu/src/com/liuheng/util/CodeUtil.java
@@ -0,0 +1,8 @@
+package com.liuheng.util;
+
+public class CodeUtil {
+
+    public static String getCode() {
+        return "123123";
+    }
+}
